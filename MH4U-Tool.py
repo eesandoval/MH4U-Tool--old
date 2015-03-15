2bb4a9431e09f48beced4762e6032381f0c3481d
Enrique Sandoval <ee.sandoval.jr@gmail.com>
2015-03-15 04:16:30 -0700

    Working system, major overhaul
    Added sharpness, gloves (latent power and challenger) and now properly
    prints the damage (from element, raw, and combined)

--- MH4U-Tool.py
+++ MH4U-Tool.py
@@ -6,12 +6,24 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
+        self.modifiers = {"Hammers":5.2, "HuntingHorns":5.2, "SwitchAxes":5.4,
+                          "GreatSwords":4.2, "ChargeBlades":3.6, "LongSwords":3.3,
+                          "InsectGlaives":3.1, "Lances":2.3, "Gunlances":2.3,
+                          "HeavyBowguns":1.5, "SwordnShields":1.4, "DualBlades":1.4,
+                          "LightBowguns":1.3, "Bow":1.2}
+        self.rawSharpnessLevels = {"Purple":1.45, "White":1.32, "Blue":1.20,
+                                   "Green":1.05, "Yellow":1.00, "Orange":0.75,
+                                   "Red":0.50}
+        self.eleSharpnessLevels = {"Purple":1.20, "White":1.12, "Blue":1.06,
+                                   "Green":1.00, "Yellow":0.75, "Orange":0.50,
+                                   "Red":0.25}
 
     def createWidgets(self):
-        self.okButton = tk.Button(self, text="OK", command=self.calculateDamage)
-        self.okButton.pack(side="left")
+        self.calculateButton = tk.Button(self, text="Calculate!", command=self.calculateDamage)
+        self.calculateButton.pack(side="left")
         self.createList()
         self.createSharpnessButtons()
+        self.createGloveButtons()
 
     def createList(self):
         optionList = []
@@ -24,19 +36,43 @@ class Application(tk.Frame):
 
     def createSharpnessButtons(self):
         self.chosenSharpness = tk.StringVar()
-        sharpnessLevels = ["Red", "Orange", "Yellow", "Blue", "White", "Purple"]
-        self.chosenSharpness.set("Purple")
+        sharpnessLevels = ["sharpness", "sharpness+1"]
+        self.chosenSharpness.set("sharpness")
         for sharpness in sharpnessLevels:
             b = tk.Radiobutton(self, text = sharpness, variable = self.chosenSharpness, value = sharpness)
             b.pack(side="left")
+            
+    def createGloveButtons(self):
+        self.chosenGlove = tk.StringVar()
+        gloves = ["Latent Power+2", "Challenger+2", "None"]
+        self.chosenGlove.set("None")
+        for glove in gloves:
+            b = tk.Radiobutton(self, text = glove, variable = self.chosenGlove, value = glove)
+            b.pack(side="bottom")
         
     def calculateDamage(self):
         weapon = self.chosenWeapon.get()
-        print("You chose sharpness level", self.chosenSharpness.get())
-        c.execute("SELECT * FROM ChargeBlades WHERE name='" + weapon + "'")
+        c.execute("SELECT * FROM ChargeBlades WHERE name=\"" + weapon + "\"")
         row = c.fetchone()
-        print((row['attack'] * (1 + 0.25 * row['affinity']) *
-              0.2 * 0.35 * 1.44) + (row['special attack']/100 * 0.2 * 1.2))
+        
+        rawSharpness = self.rawSharpnessLevels[row[self.chosenSharpness.get()]]
+        eleSharpness = self.eleSharpnessLevels[row[self.chosenSharpness.get()]]
+        affinity = row['affinity']
+        trueRaw = row['attack']/self.modifiers['ChargeBlades']
+        
+        if (self.chosenGlove.get() == "Latent Power+2"):
+            affinity += 0.50
+        elif (self.chosenGlove.get() == "Challenger+2"):
+            trueRaw += 25
+            affinity += 0.20
+        if (affinity > 1):
+            affinity = 1
+
+        print("Damage from raw:", (trueRaw * (1 + 0.25 * affinity) * 0.2 * 0.35 * rawSharpness))            
+        print("Damage from element:", (row['special attack']/10 * 0.2 * eleSharpness))
+        print("Total damage:", (trueRaw * (1 + 0.25 * affinity) * 0.2 * 0.35 * rawSharpness) +
+              (row['special attack']/10 * 0.2 * eleSharpness))
+
 
 if __name__ == '__main__':
     conn = sql.connect("MH4U.db")
